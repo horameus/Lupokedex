0236f8c74341750dc430ce8df2ed07938bdec1c4
horameus <ludo.hartmann@gmail.com>
2024-07-23 22:47:35 +0200

    init vitest + refactor

--- src/components/LupoButton/LupoButton.tsx
+++ src/components/LupoButton/LupoButton.tsx
@@ -1,11 +1,13 @@
-import { LupoButtonProps } from '@/@types';
-import { handleClassNames } from './utils';
+import { useClassNames } from '@/utils/classNames';
+import { LupoButtonProps } from './types';
 
 import './styles.css';
 
-export default function LupoButton(props: LupoButtonProps) {
+export default function LupoButton({ variant = 'primary', ...props }: LupoButtonProps) {
+    const classNames = useClassNames('LupoButton', { variant, ...props });
+
     return (
-        <button className={handleClassNames(props)} disabled={props.disabled} onClick={props.onClick}>
+        <button className={classNames} disabled={props.disabled} onClick={props.onClick}>
             {props.children}
         </button>
     );
---
+++ src/utils/classNames/index.ts
@@ -0,0 +1,2 @@
+export { handleClassNames } from './classNameHandler';
+export { default as useClassNames } from './useClassNames';
