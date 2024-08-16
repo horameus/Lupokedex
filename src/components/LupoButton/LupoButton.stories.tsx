26f4fd7465acf2e6f327e86311c738850481c03c
horameus <ludo.hartmann@gmail.com>
2024-08-16 22:25:26 +0200

    8_storybook: storybook working fine, no disabled prop issue..

--- src/components/LupoButton/LupoButton.stories.tsx
+++ src/components/LupoButton/LupoButton.stories.tsx
@@ -1,24 +1,44 @@
-import type { StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react';
+import '../../styles.global.css';
 import LupoButton from './LupoButton';
+import './styles.css';
 
-export const ActionsData = {
-    onclick: fn(),
-};
-
-export default {
+const meta: Meta<typeof LupoButton> = {
     component: LupoButton,
-    title: 'LupoButton',
-    tags: ['autodocs'],
-    args: {
-        ...ActionsData,
+    parameters: {
+        layout: 'centered',
     },
 };
+export default meta;
 
 type Story = StoryObj<typeof LupoButton>;
 
 export const Primary: Story = {
     args: {
+        children: 'Search',
         variant: 'primary',
+    },
+};
+
+export const Secondary: Story = {
+    args: {
+        children: 'Search',
+        variant: 'secondary',
+    },
+};
+
+export const PrimaryDisabled: Story = {
+    args: {
+        children: 'Search',
+        variant: 'primary',
+        disabled: true,
+    },
+};
+
+export const SecondaryDisabled: Story = {
+    args: {
+        children: 'Search',
+        variant: 'secondary',
+        disabled: true,
     },
 };
