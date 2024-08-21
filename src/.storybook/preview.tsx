919a739fbf417b6f57387a385b7583e97ee71e64
Ludovic Hartmann <131392547+horameus@users.noreply.github.com>
2024-08-21 20:34:45 +0200

    Release/0.1.0/8 storybook (#11)
    * 8_storybook: init storybook and its configuration
    
    * 8_storybook: storybook working fine, no disabled prop issue..
    
    * 8_storybook: fixed disabled props issue + unit test
    
    ---------
    
    Co-authored-by: BenoitSafari <gregory.michalak.192@gmail.com>

---
+++ src/.storybook/preview.tsx
@@ -0,0 +1,32 @@
+import { Preview, StoryFn } from '@storybook/react';
+import React from 'react';
+import '../styles.global.css';
+
+const Decorator = (Story: StoryFn) => {
+    return (
+        <div style={{ boxSizing: 'border-box', display: 'flex', padding: '2rem' }}>
+            <Story />
+        </div>
+    );
+};
+
+const preview: Preview = {
+    decorators: [Decorator],
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+        layout: 'fullscreen',
+        options: {
+            storySort: {
+                order: ['*'],
+            },
+        },
+    },
+};
+
+export default preview;
